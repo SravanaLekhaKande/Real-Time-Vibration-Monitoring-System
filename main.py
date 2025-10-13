20ebee7657eb890d1acc303af8e7cbfd92541eee
SravanaLekhaKande <sravanalekhakande15@gmail.com>
2025-10-12 20:53:06 -0500

    Comments in main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+Main entry point for the Real Time Vibration Monitoring System GUI application.
+
+This script initializes the PyQt application and launches the main window.
+It creates a QApplication instance which manages the event loop for user
+interaction. The VibrationMonitorGUI window is created and shown, and the
+application enters its main event loop, waiting for events such as button
+clicks or any uuser actions.
+
+When the GUI is closed, the application exits cleanly.
+"""
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui import VibrationMonitorGUI
