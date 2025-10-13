58de2b6e96bf91c90eda0d03f3dbf1910eb01670
SravanaLekhaKande <sravanalekhakande15@gmail.com>
2025-10-12 20:25:51 -0500

    Update comments in config.py

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 class Config:
+    # Number of sensors going to be simulated/displayed in the GUI.
     SENSOR_COUNT = 4
-    SAMPLE_RATE = 1000  # Hz
-    BUFFER_LEN = 1024   # Samples per update
+
+    # Data sampling frequency in Hz (samples per second).
+    SAMPLE_RATE = 1000
+
+    # Buffer size for each data update - big enough for good FFT, small enough to update the GUI smoothly.
+    BUFFER_LEN = 1024
+
+    # Default vibration limits per sensor to trigger alerts.
     DEFAULT_THRESHOLDS = [1.0, 2.0, 2.0, 3.0]
